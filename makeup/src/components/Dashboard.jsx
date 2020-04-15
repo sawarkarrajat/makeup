a80656345601f1418aadc95303e97468da04a551
Rajat Sawarkar <rajat.sawarkar@gmail.com>
2020-04-16 00:50:59 +0530

    added a button for switching

--- makeup/src/components/Dashboard.jsx
+++ makeup/src/components/Dashboard.jsx
@@ -1,38 +1,57 @@
 import React from "react";
 import "../sass/Dashboard.sass";
 import Collage from "./Collage";
+import Button from "@material-ui/core/Button";
 
 const Navbar = () => {
-  return (
-    <div className="navcontainer">
-      <div className="title">
-        <h1>Makeup</h1>
-      </div>
-      <div className="menu">
-        <span id="mtxt">brands</span>
-        <span id="mtxt">lipstick</span>
-        <span id="mtxt">nail polish</span>
-      </div>
-    </div>
-  );
+	return (
+		<div className="navcontainer">
+			<div className="title">
+				<h1>Makeup</h1>
+			</div>
+			<div className="menu">
+				<span id="mtxt">brands</span>
+				<span id="mtxt">lipstick</span>
+				<span id="mtxt">nail polish</span>
+			</div>
+		</div>
+	);
 };
 
 function Dashboard() {
-  return (
-    <div className="main">
-      <Navbar />
+	// let filter = false;
+	let filter = true;
+	return (
+		<div className="main">
+			<Navbar />
 
-      <div className="container">
-        <div className="rowgrid">
-          <div className="filter"></div>
-          <div className="collage">
-            <Collage />
-          </div>
-          <div className="search"></div>
-        </div>
-      </div>
-    </div>
-  );
+			<div className="container">
+				{filter ? (
+					<div className="rowgrid">
+						<div className="filterContainer">
+							<Button id="btn" variant="contained" size="small">
+								&#60;
+							</Button>
+						</div>
+						<div className="collage">
+							<Collage />
+						</div>
+					</div>
+				) : (
+					<div className="rowgrid">
+						<div className="collage">
+							<Collage />
+						</div>
+						<div className="searchContainer">
+							<Button id="btn" variant="contained" size="small">
+								&#62;
+							</Button>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Dashboard;
