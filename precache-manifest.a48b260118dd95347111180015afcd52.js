86df86c8b56d63b4d6ab26a1dea6a9544415df25
Rajat Sawarkar <rajat.sawarkar@gmail.com>
2020-10-05 12:13:30 +0530

    Updates

---
+++ precache-manifest.a48b260118dd95347111180015afcd52.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1713f7090e7a4d26b3de5b43571b50ce",
+    "url": "/makeup/index.html"
+  },
+  {
+    "revision": "0ad9b09c5f91e70a9e20",
+    "url": "/makeup/static/css/main.7523633d.chunk.css"
+  },
+  {
+    "revision": "5191a2534f8ceadb047c",
+    "url": "/makeup/static/js/2.2418969d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/makeup/static/js/2.2418969d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ad9b09c5f91e70a9e20",
+    "url": "/makeup/static/js/main.e59626ec.chunk.js"
+  },
+  {
+    "revision": "ae05fbb84b31bc68c550",
+    "url": "/makeup/static/js/runtime-main.742bb0aa.js"
+  },
+  {
+    "revision": "0ce9e4cb96c111730134f955202868ac",
+    "url": "/makeup/static/media/bkgnd.0ce9e4cb.jpg"
+  },
+  {
+    "revision": "0f4e10b272cfb74c31ee3ccd03ec5901",
+    "url": "/makeup/static/media/girl.0f4e10b2.png"
+  },
+  {
+    "revision": "06f746ba3ae1a8e3c1f0058c7d525e4c",
+    "url": "/makeup/static/media/girlhhmm.06f746ba.png"
+  }
+]);
