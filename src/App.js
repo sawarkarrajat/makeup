ccb27e3f23e7229c5b6e7f8843fbdb3ecc8f1436
Rajat Sawarkar <rajat.sawarkar@gmail.com>
2020-10-05 12:57:33 +0530

    deploy trial 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import "./App.sass";
 /**
@@ -10,14 +10,14 @@ import "./App.sass";
  * @property {function}
  */
 function App() {
-	return (
-		<div className="App">
-			<div className="app__back"></div>
-			<Router>
-				<Route path="/" exact component={Dashboard}></Route>
-			</Router>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="app__back"></div>
+      <Router>
+        <Route path="/makeup" exact component={Dashboard}></Route>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
